8f63cec90a12dd1bd9846073001eede388ae212b
nimal7sen-code <nimal7sen@gmail.com>
2026-02-06 13:50:53 +0530

    Add files via upload

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-Public class SampleProgram {
+public class SampleProgram {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your name: ");
